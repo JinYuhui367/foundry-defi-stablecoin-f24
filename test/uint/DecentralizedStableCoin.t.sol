52ce95beec9f1fe058e3645eb17615f8b687ae6c
jyh-centos9 <568000273@qq.com>
2024-09-20 11:56:20 +0800

    going to code some invariant test (p18), adding some functions in DSCEngine from github repo

--- test/uint/DecentralizedStableCoin.t.sol
+++ test/uint/DecentralizedStableCoin.t.sol
@@ -137,6 +137,38 @@ contract DSCEngineTest is Test {
     }
 
     ///////////////////
+    // GitHub Tests //
+    ///////////////////
+    function testCantMintToZeroAddress() public {
+        vm.startPrank(dsc.owner());
+        vm.expectRevert();
+        dsc.mint(address(0), 100);
+        vm.stopPrank();
+    }
+
+    function testMustMintMoreThanZero() public {
+        vm.prank(dsc.owner());
+        vm.expectRevert();
+        dsc.mint(address(this), 0);
+    }
+
+    function testMustBurnMoreThanZero() public {
+        vm.startPrank(dsc.owner());
+        dsc.mint(address(this), 100);
+        vm.expectRevert();
+        dsc.burn(0);
+        vm.stopPrank();
+    }
+
+    function testCantBurnMoreThanYouHave() public {
+        vm.startPrank(dsc.owner());
+        dsc.mint(address(this), 100);
+        vm.expectRevert();
+        dsc.burn(101);
+        vm.stopPrank();
+    }
+
+    ///////////////////
     // ChatGPT Tests //
     ///////////////////
 
