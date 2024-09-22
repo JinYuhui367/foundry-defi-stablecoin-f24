4bed37138b429e7b9d78980eded446adf4185fe4
jyh-centos9 <568000273@qq.com>
2024-09-22 13:08:30 +0800

    deploy to sepolia successfully

--- src/libraries/OracleLib.sol
+++ src/libraries/OracleLib.sol
@@ -25,9 +25,17 @@ library OracleLib {
         (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound) =
             pricefeed.latestRoundData();
 
+        if (updatedAt == 0 || answeredInRound < roundId) {
+            revert OracleLib__StalePrice();
+        }
+
         uint256 secondsSince = block.timestamp - updatedAt; // 计算自上次更新后的秒数
         if (secondsSince > TIMEOUT) revert OracleLib__StalePrice(); // 若价格过期，抛出错误
 
         return (roundId, answer, startedAt, updatedAt, answeredInRound); // 返回最新的价格数据
+    }
+
+    function getTimeout(AggregatorV3Interface /* chainlinkFeed */ ) public pure returns (uint256) {
+        return TIMEOUT;
     }
 }
--- test/fuzz/continueOnRevert/Handler.t.sol
+++ test/fuzz/continueOnRevert/Handler.t.sol
@@ -4,10 +4,10 @@ pragma solidity ^0.8.18;
 
 import {Test, console} from "forge-std/Test.sol";
 import {StdInvariant} from "forge-std/StdInvariant.sol";
-import {DeployDSC} from "../../script/DeployDSC.s.sol";
-import {DSCEngine} from "../../src/DSCEngine.sol";
-import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
-import {HelpConfig} from "../../script/HelpConfig.s.sol";
+import {DeployDSC} from "../../../script/DeployDSC.s.sol";
+import {DSCEngine} from "../../../src/DSCEngine.sol";
+import {DecentralizedStableCoin} from "../../../src/DecentralizedStableCoin.sol";
+import {HelpConfig} from "../../../script/HelpConfig.s.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
 
